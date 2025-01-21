cf4fa56e1fa450f200d06608f7c246d7617adb28
ngoc-main <dangngoc4332@gmail.com>
2025-01-21 22:46:33 +0700

    Configured routes

--- spaservicefe/src/App.js
+++ spaservicefe/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg'
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/loginPage/loginPage.tsx'
+import RegisterPage from './pages/registerPage/registerPage.tsx'
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-      <div className='flex h-screen flex-col items-center justify-center bg-gray-100'>
-        <h1 className='text-3xl font-bold text-blue-500'>Hello, Tailwind CSS & SCSS!</h1>
-        <button className='custom-button mt-4'>Click Me</button>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes >
+        <Route path='/'>
+          <Route index />
+          <Route path='login' element={<LoginPage />} />
+          <Route path='register' element={<RegisterPage />} />
+          <Route path='services' />
+          <Route path='services/:id' />
+          <Route path='reset-password' />
+          <Route path='profile' />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
