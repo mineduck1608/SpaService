ab1ed1e89ecd8f2dfcd90d7e2da0aa1a140bf5c1
Dang Minh Duc <ducdm160804@gmail.com>
2025-01-22 21:33:27 +0700

    edit account controller

--- SpaServiceBE/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ SpaServiceBE/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c961eea579aa3292f9683608feb607f67fd4869")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27347cb77bc39b795f4e314284bc7da6435a37a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
