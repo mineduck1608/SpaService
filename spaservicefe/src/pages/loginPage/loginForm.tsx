a02d8b4ad16ea92d875716acf4c4e29e612861fc
Dang Minh Duc <ducdm160804@gmail.com>
2025-01-22 21:43:20 +0700

    123

--- spaservicefe/src/pages/loginPage/loginForm.tsx
+++ spaservicefe/src/pages/loginPage/loginForm.tsx
@@ -37,14 +37,14 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className='p-6 md:p-8 justify-center content-center' onSubmit={submit}>
+          <form className='p-6 md:p-8' onSubmit={submit}>
             <div className='w-full flex justify-center'>
-              <img src={logo} className='w-1/2 translate-y-10' />
+              <img src={logo} className='w-1/4 translate-y-2' />
             </div>
-            <div className='flex flex-col gap-6'>
+            <div className='flex flex-col gap-6 mt-2'>
               <div className='flex flex-col items-center text-center'>
                 <h1 className='text-2xl font-bold'>Welcome back</h1>
-                <p className='text-balance text-muted-foreground'>Login to your Acme Inc account</p>
+                <p className='text-balance text-muted-foreground'>Login to use our service</p>
               </div>
               <div className='grid gap-2'>
                 <Label htmlFor='email'>Username</Label>
