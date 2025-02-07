48bf28af4e022713c181023d151ba77561f6d537
Dang Minh Duc <ducdm160804@gmail.com>
2025-02-07 16:25:32 +0700

    changes

--- spaservicefe/src/App.js
+++ spaservicefe/src/App.js
@@ -9,31 +9,93 @@ import Header from './components/ui/header.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import React from 'react'
 
+function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
 
 function App() {
   return (
     <GoogleOAuthProvider clientId='397904889849-udf1t7mvf7vmr1bvvdbmv2amj0nea404.apps.googleusercontent.com'>
       <BrowserRouter>
         <Routes>
-          <Route path='/'>
-            <Route index />
-            <Route path='footer' element={<Footer />}/>
-            <Route path='login' element={<LoginPage />} />
-            <Route path='register' element={<RegisterPage />} />
-            <Route path='/media/:tab' element={<MediaPage />} />
-            <Route path='services' />
-            <Route path='services/:id' />
-            <Route path='reset-password' />
-            <Route path='profile' />
-            <Route path='intro' element={<AboutUsPage />} />
-            <Route path='footer' element={ 
-              <>
-              <Footer />
-              <AboutUsPage />
-              </>
-              }/>
-            <Route path='header' element={<Header />} />
-          </Route>
+          <Route
+            path='/'
+            element={
+              <Layout>
+                <div>Home Page</div>
+              </Layout>
+            }
+          />
+          <Route
+            path='login'
+            element={
+              <Layout>
+                <LoginPage />
+              </Layout>
+            }
+          />
+          <Route
+            path='register'
+            element={
+              <Layout>
+                <RegisterPage />
+              </Layout>
+            }
+          />
+          <Route
+            path='/media/:tab'
+            element={
+              <Layout>
+                <MediaPage />
+              </Layout>
+            }
+          />
+          <Route
+            path='services'
+            element={
+              <Layout>
+                <div>Services Page</div>
+              </Layout>
+            }
+          />
+          <Route
+            path='services/:id'
+            element={
+              <Layout>
+                <div>Service Details</div>
+              </Layout>
+            }
+          />
+          <Route
+            path='reset-password'
+            element={
+              <Layout>
+                <div>Reset Password Page</div>
+              </Layout>
+            }
+          />
+          <Route
+            path='profile'
+            element={
+              <Layout>
+                <div>Profile Page</div>
+              </Layout>
+            }
+          />
+          <Route
+            path='intro'
+            element={
+              <Layout>
+                <AboutUsPage />
+              </Layout>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </GoogleOAuthProvider>
