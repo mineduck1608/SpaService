f81e2855fe3c0bd8bbf1ba4a7c36d56e0548c7bc
ngoc-main <dangngoc4332@gmail.com>
2025-02-12 20:22:03 +0700

    Add long detail

--- spaservicefe/src/pages/detailPage/detailPage.tsx
+++ spaservicefe/src/pages/detailPage/detailPage.tsx
@@ -21,18 +21,43 @@ export default function DetailPage() {
           &nbsp;&gt; AAA
         </span>
       </div>
-      <div className='flex w-full justify-center bg-red-200 pt-5'>
-        <div className='w-5/6 shadow-lg lg:w-3/4'>
-          {/* Short intro and img */}
-          <div className='flex justify-between'>
-            <div className='w-[49.5%] bg-blue-200'>
-              <StockImg />
+      <div>
+        <div className='flex w-full justify-center bg-red-200 pt-5'>
+          {/* Detail is here */}
+          <div className='w-11/12 shadow-lg lg:w-3/4'>
+            {/* Short intro and img */}
+            <div className='flex justify-between'>
+              <div className='w-[49.5%] bg-blue-200'>
+                <StockImg />
+              </div>
+              <div className='w-[49.5%] bg-gray-200'>
+                <ShortDetail d={data} />
+              </div>
             </div>
-            <div className='w-[49.5%] bg-gray-200'>
-              <ShortDetail d={data} />
+            {/* Service intro */}
+            <div className='w-full bg-teal-200 p-2 mb-20'>
+              <p className='p-3 text-center text-2xl font-bold'>Service introduction</p>
+              {/* Sample text */}
+              <div >
+                <div id="lipsum">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id neque ultrices, ullamcorper diam id, porta libero. Nulla pellentesque fermentum egestas. Ut ultrices, justo et vehicula tempus, enim tellus mollis ligula, quis luctus sapien est vitae dui. Proin consectetur, nunc non varius hendrerit, lacus nibh tincidunt nibh, feugiat dictum elit dolor in tortor. Nullam aliquet magna ut sapien egestas, vel hendrerit arcu facilisis. Mauris nec arcu non lectus tempor posuere ac sit amet est. Nam euismod lacus at libero rhoncus ultricies. Maecenas ipsum massa, tempor sit amet sagittis tempus, suscipit in urna. Vestibulum at purus nisi. Praesent diam ligula, consequat non nunc eget, mattis maximus est. Proin cursus, quam a consequat mollis, nisl risus congue eros, eget finibus sem tortor et dolor. Quisque sit amet tempor felis, eget auctor sapien. Suspendisse molestie eleifend diam ut ullamcorper. Proin ac ultrices nisi, at euismod leo.
+                  </p>
+                  <p>
+                    Fusce id tellus congue, dapibus enim id, sodales metus. Nunc hendrerit leo nec convallis facilisis. Fusce consectetur tellus eu dolor interdum, ac mattis dolor malesuada. Sed luctus nulla id sem mollis, in finibus nunc fermentum. Nunc id mollis ipsum. In ac velit lobortis, hendrerit elit non, vulputate tortor. Aliquam sodales at nisi id sodales. Phasellus congue nunc id sollicitudin sodales. Proin nec lectus a nisi porta venenatis.
+                  </p>
+                  <p>
+                    Proin sit amet semper libero. Praesent in turpis lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent pulvinar nisl nec odio placerat vestibulum. Curabitur sodales libero nec aliquam pretium. Duis ac lobortis metus. Mauris neque nisi, imperdiet vitae dignissim nec, commodo at eros. Nam vel ultrices mauris. Donec urna justo, vulputate sit amet euismod ac, pulvinar convallis ipsum. Sed in dui leo. In hac habitasse platea dictumst. Fusce vestibulum tempus ipsum, in convallis diam vulputate ac. Aenean metus massa, tempus nec tempus vel, ornare a elit.
+                  </p>
+                  <p>
+                    Donec pretium rhoncus turpis quis euismod. Morbi consequat luctus felis vitae mollis. Quisque quam ligula, accumsan id pharetra eu, condimentum quis elit. Phasellus ante justo, consectetur vel suscipit sit amet, auctor a felis. Aliquam sit amet magna tincidunt, pulvinar augue vel, dignissim metus. Vivamus accumsan non leo eu accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam eget metus pretium, placerat leo et, suscipit massa. Aenean varius turpis nec nulla dapibus, eu congue purus ultrices. Phasellus volutpat, leo in porta tempor, erat nibh pulvinar metus, at efficitur augue metus a odio. Etiam in efficitur turpis. Proin laoreet porta tristique. Morbi ut porttitor dui. Donec ut ex in enim gravida cursus. Sed elementum, leo sed placerat egestas, urna nibh mattis turpis, nec rhoncus eros erat vel orci. Aenean sollicitudin nisi tempor nisi sodales posuere.
+                  </p>
+                  <p>
+                    Quisque porttitor at diam vitae placerat. Donec euismod neque in justo placerat dapibus. Proin finibus, dolor bibendum viverra consectetur, lorem neque pulvinar elit, vitae auctor urna urna ac leo. Phasellus ac suscipit velit, non malesuada ante. Ut fringilla est non lorem posuere pellentesque. Aenean vel diam dapibus, vestibulum enim pellentesque, gravida libero. Morbi luctus nisi sit amet tellus euismod lobortis. Nulla facilisi. Integer in leo lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam lectus purus, maximus eu ligula non, condimentum gravida erat. Etiam efficitur congue urna, ut lobortis neque fringilla id. Integer porttitor eros est, sed congue sapien accumsan vel.
+                  </p></div>
+              </div>
             </div>
           </div>
-          {/* Long description... which is missing :( */}
         </div>
       </div>
     </div>
