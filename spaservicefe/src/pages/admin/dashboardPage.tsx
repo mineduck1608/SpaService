fb2243ea3a0628369aeb52ac29390d500f7fa1e3
ngoc-main <dangngoc4332@gmail.com>
2025-02-14 14:06:39 +0700

    Revert "Work on bookmark side bar task"
    This reverts commit 24f60ec15729cd3d6ca5066cc35985d5568239d0.

--- spaservicefe/src/pages/admin/dashboardPage.tsx
+++ spaservicefe/src/pages/admin/dashboardPage.tsx
@@ -6,19 +6,17 @@ import { BarChartComp } from 'src/components/bar-chart'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage } from 'src/components/ui/breadcrumb'
 import { Separator } from 'src/components/ui/separator'
 import { SidebarInset, SidebarProvider, SidebarTrigger } from 'src/components/ui/sidebar'
-import { getFav, sideData } from './sidebar.util'
-import { useState } from 'react'
+import { sideData } from './sidebar.util'
 
 export default function Page() {
-  const [data, setData] = useState(sideData)
   
   return (
     <SidebarProvider>
-      <SidebarLeft
-        favourite={[]}
-        main={data.workspaces}
-        header={data.navMain} props={{}}
-        secondary={data.navSecondary}
+      <SidebarLeft 
+      favourite={[]} 
+      main={sideData.workspaces} 
+      header={sideData.navMain} props={{}} 
+      secondary={sideData.navSecondary}
       />
       <SidebarInset>
         <header className='sticky top-0 flex h-14 shrink-0 items-center gap-2 bg-background'>
