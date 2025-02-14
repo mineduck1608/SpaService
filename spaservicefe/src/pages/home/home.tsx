2299766026985c405ffa4176b39e418d827316a8
Dang Minh Duc <ducdm160804@gmail.com>
2025-02-14 07:51:48 +0700

    fix image transition

--- spaservicefe/src/pages/home/home.tsx
+++ spaservicefe/src/pages/home/home.tsx
@@ -1,112 +1,129 @@
-import React, { useState, useEffect } from "react";
-import Main1 from "../../images/home/Main1.png";
+import React, { useState, useEffect } from 'react';
+import Main1 from '../../images/home/Main1.png';
 import Main2 from '../../images/home/Main2.png';
 import Main3 from '../../images/home/Main3.png';
 
 const images = [Main1, Main2, Main3];
 
 const Home = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [isTextVisible, setIsTextVisible] = useState(true);
-    const [isAutoPlaying, setIsAutoPlaying] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isTextVisible, setIsTextVisible] = useState(true);
+  const [isAutoPlaying, setIsAutoPlaying] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState(false); // Thêm trạng thái để theo dõi quá trình tải hình ảnh
 
-    // Thêm useEffect để preload hình ảnh
-    useEffect(() => {
-        images.forEach((imageUrl) => {
-            const img = new Image();
-            img.src = imageUrl;
-        });
-    }, []);
-
-    useEffect(() => {
-        let intervalId: NodeJS.Timeout;
-        
-        if (isAutoPlaying) {
-            intervalId = setInterval(() => {
-                setIsTextVisible(false);
-                setTimeout(() => {
-                    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-                    setIsTextVisible(true);
-                }, 500);
-            }, 3000);
-        }
-
-        return () => {
-            if (intervalId) {
-                clearInterval(intervalId);
-            }
+  // Thêm useEffect để preload hình ảnh
+  useEffect(() => {
+    const loadImages = () => {
+      let loadedCount = 0;
+      images.forEach((imageUrl) => {
+        const img = new Image();
+        img.src = imageUrl;
+        img.onload = () => {
+          loadedCount += 1;
+          if (loadedCount === images.length) {
+            setImagesLoaded(true); // Khi tất cả hình ảnh đã tải xong, cập nhật trạng thái
+          }
         };
-    }, [isAutoPlaying]);
+        img.onerror = () => {
+          console.error(`Không thể tải hình ảnh: ${imageUrl}`);
+        };
+      });
+    };
+    loadImages();
+  }, []);
 
-    const nextImage = () => {
-        setIsAutoPlaying(false);
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (isAutoPlaying && imagesLoaded) {
+      intervalId = setInterval(() => {
         setIsTextVisible(false);
         setTimeout(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-            setIsTextVisible(true);
+          setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+          setIsTextVisible(true);
         }, 500);
+      }, 3000);
+    }
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
     };
+  }, [isAutoPlaying, imagesLoaded]);
 
-    const prevImage = () => {
-        setIsAutoPlaying(false);
-        setIsTextVisible(false);
-        setTimeout(() => {
-            setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-            setIsTextVisible(true);
-        }, 500);
-    };
+  const nextImage = () => {
+    setIsAutoPlaying(false);
+    setIsTextVisible(false);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setIsTextVisible(true);
+    }, 500);
+  };
 
-    return (
-        <main className='overflow-x-hidden font-montserrat'>
-            {/* Slider Section */}
-            <div className="relative w-full h-[100vh] overflow-hidden">
-                <div
-                    className="w-full h-full bg-cover bg-center transition-all duration-500 ease-in-out"
-                    style={{
-                        backgroundImage: `url(${images[currentIndex]})`,
-                    }}
-                ></div>
-                
-                {/* Text Overlay với animation fade up/down */}
-                <div 
-                    className={`absolute top-[58%] left-64 transform -translate-y-1/2 text-white z-10 transition-all duration-500 ease-in-out
-                    ${isTextVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-full'}`}>
-                    <h1 className="text-7xl font-light mb-4">Sen Spa</h1>
-                    <p className="text-2xl mb-2">Luxurious space, </p>
-                    <p className="text-2xl mb-8">blending modernity and classic elegance.</p>
-                    <button className="px-8 py-3 bg-[#a040a0] text-white rounded-tl-[1rem] rounded-br-[1rem] hover:bg-[#8a3b8a] transition-colors duration-1000">
-                        Explore <span className="ml-2">›</span>
-                    </button>
-                </div>
+  const prevImage = () => {
+    setIsAutoPlaying(false);
+    setIsTextVisible(false);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+      setIsTextVisible(true);
+    }, 500);
+  };
 
-                {/* Navigation buttons */}
-                <div className="absolute top-1/2 w-full flex justify-between transform -translate-y-1/2 z-10 px-24">
-                    <button
-                        onClick={prevImage}
-                        className="bg-[#a040a0] text-white text-lg cursor-pointer hover:bg-[#8a3b8a] transition-colors duration-700 border-none rounded-tl-[1rem] rounded-br-[1rem] mx-4 flex items-center justify-center w-10 h-10"
-                    >
-                        <span>←</span>
-                    </button>
-                    <button
-                        onClick={nextImage}
-                        className="bg-[#a040a0] text-white text-lg cursor-pointer hover:bg-[#8a3b8a] transition-colors duration-700 border-none rounded-tl-[1rem] rounded-br-[1rem] mx-4 flex items-center justify-center w-10 h-10"
-                    >
-                        <span>→</span>
-                    </button>
-                </div>
-                <div className="absolute bottom-5 w-full flex justify-center gap-2.5">
-                    {images.map((_, index) => (
-                        <button
-                            key={index}
-                            className={`w-2.5 h-2.5 rounded-full border-none cursor-pointer
+  return (
+    <main className='overflow-x-hidden font-montserrat'>
+      {/* Slider Section */}
+      <div className='relative w-full h-[100vh] overflow-hidden'>
+        {imagesLoaded && ( // Chỉ hiển thị slider khi tất cả hình ảnh đã được tải
+          <div
+            className='w-full h-full bg-cover bg-center transition-all duration-500 ease-in-out'
+            style={{
+              backgroundImage: `url(${images[currentIndex]})`
+            }}
+          ></div>
+        )}
+
+        {/* Text Overlay với animation fade up/down */}
+        <div
+          className={`absolute top-[58%] left-64 transform -translate-y-1/2 text-white z-10 transition-all duration-500 ease-in-out
+                    ${isTextVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-full'}`}
+        >
+          <h1 className='text-7xl font-light mb-4'>Sen Spa</h1>
+          <p className='text-2xl mb-2'>Luxurious space, </p>
+          <p className='text-2xl mb-8'>blending modernity and classic elegance.</p>
+          <button className='px-8 py-3 bg-[#a040a0] text-white rounded-tl-[1rem] rounded-br-[1rem] hover:bg-[#8a3b8a] transition-colors duration-1000'>
+            Explore <span className='ml-2'>›</span>
+          </button>
+        </div>
+
+        {/* Navigation buttons */}
+        <div className='absolute top-1/2 w-full flex justify-between transform -translate-y-1/2 z-10 px-24'>
+          <button
+            onClick={prevImage}
+            className='bg-[#a040a0] text-white text-lg cursor-pointer hover:bg-[#8a3b8a] transition-colors duration-700 border-none rounded-tl-[1rem] rounded-br-[1rem] mx-4 flex items-center justify-center w-10 h-10'
+          >
+            <span>←</span>
+          </button>
+          <button
+            onClick={nextImage}
+            className='bg-[#a040a0] text-white text-lg cursor-pointer hover:bg-[#8a3b8a] transition-colors duration-700 border-none rounded-tl-[1rem] rounded-br-[1rem] mx-4 flex items-center justify-center w-10 h-10'
+          >
+            <span>→</span>
+          </button>
+        </div>
+        <div className='absolute bottom-5 w-full flex justify-center gap-2.5'>
+          {images.map((_, index) => (
+            <button
+              key={index}
+              className={`w-2.5 h-2.5 rounded-full border-none cursor-pointer
                             ${index === currentIndex ? 'bg-light' : 'bg-white/50'}`}
-                            onClick={() => setCurrentIndex(index)}
-                        ></button>
-                    ))}
-                </div>
-            </div>
-        </main>
-    );
+              onClick={() => setCurrentIndex(index)}
+            ></button>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
