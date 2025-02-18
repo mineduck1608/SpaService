e69de01ba0d0e0b35516fe211cf946194fc5018d
ngoc-main <dangngoc4332@gmail.com>
2025-02-18 10:19:24 +0700

    Restrict date picker to 30 min steps
    Also kick out of the page when user user doesn't pick a service

--- spaservicefe/src/pages/checkout/checkoutPage.tsx
+++ spaservicefe/src/pages/checkout/checkoutPage.tsx
@@ -6,6 +6,9 @@ import { Input, DatePicker } from 'antd'
 
 export default function CheckoutPage() {
   const booked = JSON.parse(sessionStorage.getItem('BOOKED') ?? '{}') as Service
+  if(!booked.serviceId){
+    window.location.assign('/services')
+  }
   const { TextArea } = Input
   const [req, setReq] = useState<SpaRequest>({
     customerId: '',
@@ -15,17 +18,29 @@ export default function CheckoutPage() {
   })
   return (
     <div className='mb-32 mt-32 flex justify-center'>
-      <div className='mt-32 flex w-3/5 justify-center'>
+      <form className='mt-32 flex w-3/5 justify-center'>
         <div className='relative w-2/3 rounded-bl-lg rounded-tl-lg p-20 shadow-lg'>
           <ServiceOverview s={booked} />
-          <div className='mb-4 flex justify-between pt-4'>
-            <label className='grid w-2/5'>
+          <div className='mb-4 gap-6 pt-4 2xl:flex 2xl:justify-between'>
+            <label className='grid 2xl:w-[45%]'>
               Date:
-              <DatePicker showTime className='mt-2 border-[1px] p-2' required />
+              <DatePicker
+                step={1800}
+                showTime
+                showHour
+                showMinute
+                showSecond={false}
+                minuteStep={30}
+                className='mt-2 border-[1px] p-2'
+                onChange={(date, dateString) => {
+                  setReq({ ...req, startTime: date?.toDate() })
+                }}
+                required
+              />
             </label>
-            <label className='grid w-2/5'>
+            <label className='grid 2xl:w-[45%]'>
               Request employee:
-              <select className='mt-2 border-[1px] p-2'>
+              <select className='mt-2 w-full border-[1px] p-2'>
                 <option hidden defaultChecked>
                   Select an employee you want
                 </option>
@@ -52,11 +67,11 @@ export default function CheckoutPage() {
         <div className='w-1/3 rounded-br-lg rounded-tr-lg bg-purple1 px-3 py-4'>
           <p className='text-white'>You can pay immediately or at 10B1 Le Thanh Ton, Ben Nghe Ward, District 1, HCMC</p>
           <div className='flex justify-around'>
-            <button className='w-1/3 rounded-br-lg rounded-tl-lg bg-white p-1 text-purple1'>Submit request</button>
+            <button type='submit' className='w-1/3 rounded-br-lg rounded-tl-lg bg-white p-1 text-purple1'>Submit request</button>
             <button className='w-1/3 rounded-br-lg rounded-tl-lg bg-white p-1 text-purple1'>Pay now</button>
           </div>
         </div>
-      </div>
-    </div>
+      </form>
+    </div >
   )
 }
