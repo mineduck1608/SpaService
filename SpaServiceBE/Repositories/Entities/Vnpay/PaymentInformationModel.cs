27283dfe5fa213684f8d2775a64a64820f83226f
Thinh <dotruongthinh2212@gmail.com>
2025-02-20 00:19:06 +0700

    tested vnpay (via postman)

--- SpaServiceBE/Repositories/Entities/Vnpay/PaymentInformationModel.cs
+++ SpaServiceBE/Repositories/Entities/Vnpay/PaymentInformationModel.cs
@@ -9,7 +9,7 @@ namespace Repositories.Entities.Vnpay
     public class PaymentInformationModel
     {
         public string OrderType { get; set; }
-        public double Amount { get; set; }
+        public decimal Amount { get; set; }
         public string OrderDescription { get; set; }
         public string Name { get; set; }
 
