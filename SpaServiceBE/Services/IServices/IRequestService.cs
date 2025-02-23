beed7e016f20de326dd1ae786166180896d521ae
ngoc-main <dangngoc4332@gmail.com>
2025-02-23 17:59:05 +0700

    Fix conflict

--- SpaServiceBE/Services/IServices/IRequestService.cs
+++ SpaServiceBE/Services/IServices/IRequestService.cs
@@ -13,5 +13,6 @@ namespace Services.IServices
         Task<bool> Update(string requestId, Request request);
         Task<bool> Delete(string requestId);
         Task<(bool roomState, int employeeState)> CheckResourceAvailable(Request q);
+        Task<(string roomId, string employeeId)> PickRandomResource(Request q, bool chooseEmployee);
     }
 }
