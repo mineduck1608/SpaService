5882aea851f220e90a8f5fc67cead3dafc6237d4
Thinh <dotruongthinh2212@gmail.com>
2025-02-24 13:46:19 +0700

    add order, get all order from customerId

--- SpaServiceBE/SpaServiceBE/Controllers/OrderDetailController.cs
+++ SpaServiceBE/SpaServiceBE/Controllers/OrderDetailController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Repositories.Entities;
 using Services.IServices;
+using System.Net.WebSockets;
 using System.Text.Json;
 
 namespace SpaServiceBE.Controllers
@@ -11,7 +12,8 @@ namespace SpaServiceBE.Controllers
     public class OrderDetailController : ControllerBase
     {
         private readonly IOrderDetailService _service;
-
+        private readonly ICosmeticProductService _productService;
+        private readonly IOrderService _orderService;
         public OrderDetailController(IOrderDetailService service)
         {
             _service = service;
@@ -40,28 +42,53 @@ namespace SpaServiceBE.Controllers
                 var jsonElement = (JsonElement)request;
 
                 // Extract data from request
+
                 float quantity = jsonElement.GetProperty("quantity").GetSingle();
-                float subtotalAmount = jsonElement.GetProperty("subtotalAmount").GetSingle();
                 string orderId = jsonElement.GetProperty("orderId").GetString();
                 string productId = jsonElement.GetProperty("productId").GetString();
+                //get cosmeticProduct
 
+                var cosmeticProduct = await _productService.GetCosmeticProductById(productId);
+                if (cosmeticProduct.IsSelling == false || cosmeticProduct.Status == false)
+                {
+                    return BadRequest(new { msg = "The product is not avaliable" });
+                }
                 // Validate input
-                if (quantity <= 0 || subtotalAmount <= 0 || string.IsNullOrEmpty(orderId) || string.IsNullOrEmpty(productId))
+
+                if (quantity <= 0 || string.IsNullOrEmpty(orderId) || string.IsNullOrEmpty(productId))
                 {
                     return BadRequest(new { msg = "Order detail information is incomplete or invalid." });
                 }
-
+                if (cosmeticProduct.Quantity - (int)quantity < 0)
+                {
+                    return BadRequest(new { msg = "Not enough stocks" });
+                }
+                float subAmount = (float)(cosmeticProduct.Quantity * cosmeticProduct.Price);
                 // Create OrderDetail object
+
                 var orderDetail = new OrderDetail
                 {
                     OrderDetailId = new Random().Next(1, int.MaxValue), // Generate unique ID
                     Quantity = quantity,
-                    SubtotalAmount = subtotalAmount,
+                    SubtotalAmount = subAmount,
                     OrderId = orderId,
                     ProductId = productId
                 };
 
                 await _service.Create(orderDetail);
+                //update Product after order created (reduce the quantity)
+
+                cosmeticProduct.Quantity -= (int)quantity;
+                await _productService.Update(cosmeticProduct);
+                //get order to update total amount
+
+                var order = await _orderService.GetOrderByIdAsync(orderId);
+                if (order != null)
+                {
+                    order.TotalAmount += subAmount;
+                    //update order total amount
+                    await _orderService.UpdateOrderAsync(orderId, order);
+                }
                 return CreatedAtAction(nameof(GetOrderDetailById), new { id = orderDetail.OrderDetailId }, orderDetail);
             }
             catch (Exception ex)
