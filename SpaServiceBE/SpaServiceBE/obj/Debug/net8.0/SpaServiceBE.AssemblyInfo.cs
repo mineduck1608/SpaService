9eb5c0908c05f5421f83bd6d72770e9b6aa8d28a
Thinh <dotruongthinh2212@gmail.com>
2025-03-16 20:24:00 +0700

    fix decline request

--- SpaServiceBE/SpaServiceBE/obj/Debug/net8.0/SpaServiceBE.AssemblyInfo.cs
+++ SpaServiceBE/SpaServiceBE/obj/Debug/net8.0/SpaServiceBE.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaServiceBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eea621164a9c611f18750d15cf96e4fd468eb4c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4261b9b9c9b91b0bab22d5eef491674d557bb463")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaServiceBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaServiceBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
