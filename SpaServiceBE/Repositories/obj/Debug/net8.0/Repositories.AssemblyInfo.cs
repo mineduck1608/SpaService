2f062262e24edf32050730b355b9ac8fb7fb4fa1
Dang Minh Duc <ducdm160804@gmail.com>
2025-03-17 09:44:32 +0700

    upload format price

--- SpaServiceBE/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ SpaServiceBE/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44cffbf7ea080ead3a28542c1c507f55b09c0514")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5acd93d1e5c04b60e7dda26840ab60c1e64e365a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
