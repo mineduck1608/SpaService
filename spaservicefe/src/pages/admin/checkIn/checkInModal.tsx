5b5f74d2a11aaecc65734f54b990690ce9ef2b4e
lonelyoggy <phuclinhvip1000@gmail.com>
2025-03-18 11:23:38 +0700

    .

--- spaservicefe/src/pages/admin/checkIn/checkInModal.tsx
+++ spaservicefe/src/pages/admin/checkIn/checkInModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Dialog, DialogContent, DialogTrigger } from 'src/components/ui/dialog'
 import { DialogTitle } from '@radix-ui/react-dialog'
 import { Button } from 'src/components/ui/button'
@@ -9,104 +9,93 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { Input } from 'src/components/ui/input'
 import { ToastContainer } from 'react-toastify'
 import { handleCheckInSubmit, getAllEmployees } from './record.util'
-import { getToken } from '../../../types/constants'
+import { getAllAccounts } from '../accounts/account.util'
+import { jwtDecode } from 'jwt-decode'
 
 const checkInSchema = z.object({
-    employeeId: z.string().nonempty('Employee ID is required'),
-    fullName: z.string().nonempty('Full Name is required'),
-    checkInTime: z.string().nonempty('Check-in Time is required')
+  employeeId: z.string().nonempty('Employee ID is required'),
+  checkInTime: z.string().nonempty('Check-in Time is required')
 })
 
 export default function CheckInModal() {
-    const [fullName, setFullName] = useState('')
-    const form = useForm<z.infer<typeof checkInSchema>>({
-        resolver: zodResolver(checkInSchema),
-        defaultValues: {
-            employeeId: '',
-            fullName: '',
-            checkInTime: new Date().toISOString().slice(0,16)
-        }
-    })
-
-    useEffect(() => {
-        const fetchEmployeeName = async () => {
-            const token = getToken()
-            const employees = await getAllEmployees()
-            const employee = employees.find(emp => emp.accountId === token)
-            if (employee) {
-                setFullName(employee.fullName)
-                form.setValue('fullName', employee.fullName)
-                form.setValue('employeeId', employee.employeeId)
-            }
-        }
-        fetchEmployeeName()
-    }, [form])
-
-    const handleSubmit = async (data: any) => {
-        await handleCheckInSubmit(data)
+  const form = useForm<z.infer<typeof checkInSchema>>({
+    resolver: zodResolver(checkInSchema),
+    defaultValues: {
+      employeeId: '',
+      checkInTime: new Date().toISOString().slice(0, 16)
     }
+  })
 
-    return (
-        <Dialog>
-            <DialogTrigger>
-                <Button variant='outline'>Check In</Button>
-            </DialogTrigger>
-            <DialogContent className='px-10'>
-                <DialogTitle className='flex justify-center'>Employee Check-In</DialogTitle>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-4'>
-                        <FormField
-                            control={form.control}
-                            name='employeeId'
-                            render={({ field }) => (
-                                <FormItem className='mt-2 grid grid-cols-4 items-center gap-4'>
-                                    <FormLabel className='text-md text-right'>Employee ID</FormLabel>
-                                    <div className='col-span-3 space-y-1'>
-                                        <FormControl>
-                                            <Input {...field} placeholder='Enter Employee ID' readOnly/>
-                                        </FormControl>
-                                        <FormMessage className='text-sm' />
-                                    </div>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name='fullName'
-                            render={({ field }) => (
-                                <FormItem className='mt-2 grid grid-cols-4 items-center gap-4'>
-                                    <FormLabel className='text-md text-right'>Full Name</FormLabel>
-                                    <div className='col-span-3 space-y-1'>
-                                        <FormControl>
-                                            <Input {...field} placeholder='Full Name' readOnly />
-                                        </FormControl>
-                                        <FormMessage className='text-sm' />
-                                    </div>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name='checkInTime'
-                            render={({ field }) => (
-                                <FormItem className='mt-2 grid grid-cols-4 items-center gap-4'>
-                                    <FormLabel className='text-md text-right'>Check-in Time</FormLabel>
-                                    <div className='col-span-3 space-y-1'>
-                                        <FormControl>
-                                            <Input {...field} type='date-time local' placeholder='Enter Check-in Time' readOnly />
-                                        </FormControl>
-                                        <FormMessage className='text-sm' />
-                                    </div>
-                                </FormItem>
-                            )}
-                        />
-                        <div className='mt-10 flex justify-end'>
-                            <Button type='submit'>Submit</Button>
-                        </div>
-                    </form>
-                </Form>
-            </DialogContent>
-            <ToastContainer />
-        </Dialog>
-    )
+  useEffect(() => {
+    const fetchEmployeeId = async () => {
+      const token = sessionStorage.getItem('token')
+      if (token) {
+        const decodedToken: any = jwtDecode(token)
+        const accountId = decodedToken.accountId
+        const accounts = await getAllAccounts()
+        const account = accounts.find(acc => acc.accountId === accountId)
+        if (account) {
+          const employees = await getAllEmployees()
+          const employee = employees.find(emp => emp.accountId === account.accountId)
+          if (employee) {
+            form.setValue('employeeId', employee.employeeId)
+          }
+        }
+      }
+    }
+    fetchEmployeeId()
+  }, [form])
+
+  const handleSubmit = async (data: any) => {
+    await handleCheckInSubmit(data)
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button variant='outline'>Check In</Button>
+      </DialogTrigger>
+      <DialogContent className='px-10'>
+        <DialogTitle className='flex justify-center'>Employee Check-In</DialogTitle>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='employeeId'
+              render={({ field }) => (
+                <FormItem className='mt-2 grid grid-cols-4 items-center gap-4'>
+                  <FormLabel className='text-md text-right'>Employee ID</FormLabel>
+                  <div className='col-span-3 space-y-1'>
+                    <FormControl>
+                      <Input {...field} placeholder='Enter Employee ID' />
+                    </FormControl>
+                    <FormMessage className='text-sm' />
+                  </div>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='checkInTime'
+              render={({ field }) => (
+                <FormItem className='mt-2 grid grid-cols-4 items-center gap-4'>
+                  <FormLabel className='text-md text-right'>Check-in Time</FormLabel>
+                  <div className='col-span-3 space-y-1'>
+                    <FormControl>
+                      <Input {...field} type='datetime-local' placeholder='Enter Check-in Time' readOnly />
+                    </FormControl>
+                    <FormMessage className='text-sm' />
+                  </div>
+                </FormItem>
+              )}
+            />
+            <div className='mt-10 flex justify-end'>
+              <Button type='submit'>Submit</Button>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+      <ToastContainer />
+    </Dialog>
+  )
 }
