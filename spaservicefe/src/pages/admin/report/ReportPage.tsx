c9033440b12b7c662b6ba8a3c63285a17424c853
ngoc-main <dangngoc4332@gmail.com>
2025-03-21 21:17:57 +0700

    Support time bounds for report

--- spaservicefe/src/pages/admin/report/ReportPage.tsx
+++ spaservicefe/src/pages/admin/report/ReportPage.tsx
@@ -1,5 +1,5 @@
 import { formatDate } from 'date-fns'
-import dayjs from 'dayjs'
+import dayjs, { Dayjs } from 'dayjs'
 import React, { useEffect, useState } from 'react'
 import { getProductStat, getRevenues, getServiceStat } from './reportPage.util'
 import { ProductStat, ServiceStat } from '@/types/statistic'
@@ -7,10 +7,9 @@ import { formatNumber } from '../../servicesPage/servicesPage.util'
 import ServiceTable from './serviceTable'
 import ProductTable from './productTable'
 import { Margin, usePDF } from 'react-to-pdf'
+import { DatePicker } from 'antd'
 
 export default function ReportPage() {
-  const lower = dayjs().add(-3, 'M').toDate()
-  const now = dayjs().toDate()
   const [revenue, setRevenue] = useState({
     service: 0,
     product: 0,
@@ -34,40 +33,75 @@ export default function ReportPage() {
     resolution: 1
   })
   const [bounds, setBounds] = useState({
-    start: dayjs(),
+    start: dayjs().add(-3, 'M'),
     end: dayjs()
   })
-  async function getData() {
-    const response = await getRevenues()
+  const [reportBounds, setReportBounds] = useState({
+    start: dayjs().add(-3, 'M'),
+    end: dayjs()
+  })
+  async function getData(lower: Dayjs, upper: Dayjs, onDone?: () => void) {
+    const response = await getRevenues(lower, upper)
     setRevenue(response)
-    const products = await getProductStat()
+    const products = await getProductStat(lower, upper)
     setProducts(products)
-    const services = await getServiceStat()
+    const services = await getServiceStat(lower, upper)
     setServices(services)
+    onDone?.()
+  }
+  async function exportPDF() {
+    setReportBounds(bounds)
+    await getData(bounds.start, bounds.end)
+    toPDF()
   }
   useEffect(() => {
     try {
-      getData()
-    } catch (e) { }
+      getData(reportBounds.start, reportBounds.end)
+    } catch (e) {}
   }, [])
   return (
     <div className='p-1'>
-      <button
-        onClick={() => {
-          toPDF()
-        }}
-        className='mb-2 rounded-sm bg-purple1 p-1 px-2 text-white'
-      >
-        Download report
-      </button>
+      <div>
+        <span>
+          From:&nbsp;
+          <DatePicker
+            maxDate={bounds.end}
+            placeholder='Start Date'
+            value={bounds.start}
+            onChange={(e) => {
+              setBounds({ ...bounds, start: e ?? dayjs() })
+            }}
+            format={{
+              format: 'DD/MM/YYYY'
+            }}
+          />
+          &nbsp;to&nbsp;
+          <DatePicker
+            minDate={bounds.start}
+            maxDate={dayjs()}
+            placeholder='End Date'
+            value={bounds.end}
+            onChange={(e) => {
+              setBounds({ ...bounds, end: e ?? dayjs() })
+            }}
+            format={{
+              format: 'DD/MM/YYYY'
+            }}
+          />
+          &nbsp;
+        </span>
+        <button onClick={exportPDF} className='mb-2 rounded-sm bg-purple1 p-1 px-2 text-white'>
+          Download report
+        </button>
+      </div>
       <div ref={targetRef} className='border-1 w-full border-black bg-white p-2'>
         <h3 className='mb-3'>1. General information</h3>
         <div className='flex w-1/2 justify-between gap-5'>
           <p>
-            Start date: <span>{formatDate(lower, 'dd/MM/yyyy')}</span>
+            Start date: <span>{formatDate(reportBounds.start.toDate(), 'dd/MM/yyyy')}</span>
           </p>
           <p>
-            End date: <span>{formatDate(now, 'dd/MM/yyyy')}</span>
+            End date: <span>{formatDate(reportBounds.end.toDate(), 'dd/MM/yyyy')}</span>
           </p>
         </div>
         <div className='flex justify-between gap-5'>
@@ -82,11 +116,11 @@ export default function ReportPage() {
           </p>
         </div>
         <div className='flex w-1/2 justify-between gap-5'>
-          <p >
-            New customers: <span>{formatDate(lower, 'dd/MM/yyyy')}</span>
+          <p>
+            New customers: <span></span>
           </p>
           <p>
-            New employees: <span>{formatDate(now, 'dd/MM/yyyy')}</span>
+            New employees: <span></span>
           </p>
         </div>
         <h3 className='mb-3'>2. Services</h3>
--- spaservicefe/src/pages/admin/report/serviceTable.tsx
+++ spaservicefe/src/pages/admin/report/serviceTable.tsx
@@ -1,3 +1,4 @@
+import { formatNumber } from '../../../pages/servicesPage/servicesPage.util'
 import { ServiceStat } from '@/types/statistic'
 import React from 'react'
 
@@ -8,10 +9,11 @@ export default function ServiceTable(params: { data: ServiceStat[] }) {
         <tr className='*:border-[1px] *:border-purple1 *:bg-purple1 *:p-2 *:pt-0 *:text-center *:text-white'>
           <th>Category</th>
           <th>Service</th>
-          <th>Requested count</th>
+          <th>Request count</th>
           <th>Appointment count</th>
           <th>Ratings</th>
           <th>Gender distribution</th>
+          <th>Revenue</th>
         </tr>
       </thead>
       <tbody>
@@ -21,7 +23,7 @@ export default function ServiceTable(params: { data: ServiceStat[] }) {
             <td className='w-[15%] '>{v.serviceName}</td>
             <td className='w-[10%] '>{v.statistic.requestCount}</td>
             <td className='w-[10%] '>{v.statistic.appointmentCount}</td>
-            <td className='w-[35%] '>
+            <td className='w-[25%] '>
               <div className='flex justify-between'>
                 {v.statistic.rating.map((v, i) => (
                   <p>
@@ -34,6 +36,7 @@ export default function ServiceTable(params: { data: ServiceStat[] }) {
               <div>Female: {v.statistic.genderCount[0]}</div>
               <div>Male: {v.statistic.genderCount[1]}</div>
             </td>
+            <td className='w-[10%]'>{formatNumber(v.statistic.revenue)}</td>
           </tr>
         ))}
       </tbody>
